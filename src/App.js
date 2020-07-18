cf938d5d5d153a1146bd71412a62e6312e3d2ca0
Tran Thi Tuyet Ngan <ngan.tran21@pn.com.vn>
2020-07-18 07:48:20 +0700

    Submit TodoList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
+import tick from './img/tick.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todoItems:[
+      { title: 'Buy bim bim', isComplete: true},
+      { title: 'Play footbal', isComplete: true},
+      { title: 'Go out with friends'}
+    ]
+  }
+  this.onKeyUp = this.onKeyUp.bind(this);
+  this.onChange = this.onChange.bind(this);
+  }
+
+  onKeyUp(event){
+    let text = event.target.value;
+    if (event.keyCode === 13)
+    {
+    if (!text){
+      return;
+    }
+    text = text.trim();
+    if (!text) { return; }
+
+    this.setState({
+      newItem: '',
+      todoItems: [
+        { title: text, isComplete: false },
+        ...this.state.todoItems
+      ]
+    });
+    } 
+  }
+
+  onItemClicked(item){
+    console.log(item);
+    return (event) => {
+      const isComplete =item.isComplete;
+      const { todoItems } = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
+    };
+  }
+  onChange(event) {
+    this.setState({
+      newItem: event.target.value
+    });
+  }
+  render(){
+    const { todoItems, newItem } = this.state;
+      return(
+        <div className="App">
+          <div className="Header">
+            <img src={tick} width={32} height={32} />
+            <input 
+              type="text" 
+              value ={ newItem } 
+              onChange = { this.onChange }
+              placeholder="Add a new item" 
+              onKeyUp = {this.onKeyUp} />
+          </div>
+            {
+              todoItems.length > 0 && todoItems.map((item,index) => 
+              <TodoItem 
+                key = {index} 
+                item= {item} 
+                onClick={this.onItemClicked(item)} 
+              />)
+            }
+            {
+              todoItems.length === 0 && 'Nothing here.'
+            }
+        </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './TodoItem.css';
+import classNames from 'classnames';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
+
+class TodoItem extends React.Component{
+    render(){
+        const {item, onClick} = this.props;
+        let url = checkImg;
+        if (item.isComplete){
+            url = checkCompleteImg;
+        }
+        return (
+            <div className={classNames('TodoItem',{
+                'TodoItem-complete': item.isComplete
+            })}>
+                <img onClick = { onClick } src={url} width={32} />
+                <p>{ this.props.item.title }</p>
+            </div>
+        );
+    }
+}
+export default TodoItem;
